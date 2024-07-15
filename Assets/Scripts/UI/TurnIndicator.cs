0bdc057ef9d8bfa7317874496810730f6b48b4b7
Ducquwee <ducquwee@gmail.com>
2024-07-15 21:13:46 +0100

    Added a gameobject to track current player

---
+++ Assets/Scripts/UI/TurnIndicator.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnIndicator : MonoBehaviour
+{
+    private TurnTracker turnTracker;
+    [SerializeField] private GameObject indicatorArrow;
+
+    private void Awake() {
+        turnTracker = FindAnyObjectByType<TurnTracker>();
+    }
+
+    public void MoveTurnIndicator() {
+        Debug.Log("moved");
+
+        Transform currentPlayerTransform = turnTracker.QueryTurn().transform;
+        indicatorArrow.transform.SetParent(currentPlayerTransform);
+
+        SpriteRenderer spriteRenderer = currentPlayerTransform.GetComponent<SpriteRenderer>();
+        float spriteHeight = spriteRenderer.bounds.size.y;
+
+        indicatorArrow.transform.localPosition = new Vector3(0, spriteHeight + 1.5f, 0);
+    }
+}
