41ac293379f4a60ca889bfd11e952f5a5ce83cd1
Ducquwee <ducquwee@gmail.com>
2024-07-26 11:49:02 +0100

    Added base main menu

---
+++ Assets/Scripts/UI/Menu/MainMenu.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame() {
+        SceneManager.LoadScene(1,LoadSceneMode.Single);
+    }
+
+    public void OpenSettings() {
+
+    }
+
+    public void CloseSettings() {
+
+    }
+
+    public void Quit() {
+
+    }
+}
